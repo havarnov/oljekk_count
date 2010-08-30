ddb4e304513a4018d18fcfaaf1b077fc44af7e83
Havar Novik <havar.novik@gmail.com>
2010-08-30 08:09:11 +0200

    change the stats function to only make UTC time list

--- count.py
+++ count.py
@@ -21,43 +21,30 @@ class count:
 
         return int(self.act_file[list][-1])
 
-    def stats(self, key, key2, value, last_count):
+    def stats(self, key, value, last_count):
         if self.act_file.has_key(key):
             self.act_key = self.act_file[key]
             self.act_value = self.act_file[value]
-            self.act_key2 = self.act_file[key2]
-            #print(self.act_key[-1])
-            last_key = int(self.act_key[-1])
-            #last_key = self.act_key[-1]
-            print(last_key)
-            last_key2 = self.act_key2[-1]
-            print(last_count,' skal vare mindre < enn ',int(last_key2) + 5*60)
-            if last_count < int(last_key2) + 5*60:
+            last_key = self.act_key[-1]
+            if last_count < int(last_key) + 5*60:
                 self.act_value[-1] += 1
                 self.act_file[value] = self.act_value
-                print('if')
             else:
-                last_key = last_key + 5
-                #self.act_key.append('{0}'.format(last_key))
                 tm = time.localtime()
                 m = tm.tm_min
                 t = tm.tm_hour
                 m = self.roundint(m,5)
-                self.act_key.append('{0}{1}'.format(t,m))
-                self.act_key2.append(time.mktime((tm.tm_year,tm.tm_mon,tm.tm_mday,t,m,0,tm.tm_wday,tm.tm_yday,1)))
+                self.act_key.append(time.mktime((tm.tm_year,tm.tm_mon,tm.tm_mday,t,m,0,tm.tm_wday,tm.tm_yday,1)))
                 self.act_value.append(1)
                 self.act_file[key] = self.act_key
-                self.act_file[key2] = self.act_key2
                 self.act_file[value] = self.act_value
-                print('else')
         else:
             tm = time.localtime()
             m = tm.tm_min
             t = tm.tm_hour
             m = self.roundint(m,5)
-            self.act_file[key] = ['{0}{1}'.format(t,m)]
             self.act_file[value] = [1]
-            self.act_file[key2] = [time.mktime((tm.tm_year,tm.tm_mon,tm.tm_mday,t,m,0,tm.tm_wday,tm.tm_yday,1))]
+            self.act_file[key] = [time.mktime((tm.tm_year,tm.tm_mon,tm.tm_mday,t,m,0,tm.tm_wday,tm.tm_yday,1))]
 
     def roundint(self,n,p):
         x = (n+p)/p
@@ -69,12 +56,15 @@ class count:
         for i in self.act_file[list]:
             print(i)
 
+    def graph(self):
+
+
 
 c = count()
 
 last = c.count('test_list','test.dat') 
 
-c.stats('test_key', 'test_key2','test_value',last)
+c.stats('test_key','test_value',last)
 
 #c.print_list('test_list')
 #c.print_list('test_value')
